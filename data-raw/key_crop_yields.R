ab41f266432ca787dd34f057c83822196c5b0405
Ruiqi Tang <rtan0006@student.monash.edu>
2021-10-21 14:22:54 +0800

    Initial commit

---
+++ data-raw/key_crop_yields.R
@@ -0,0 +1,18 @@
+## code to prepare `key_crop_yields` dataset goes here
+
+usethis::use_data(key_crop_yields, overwrite = TRUE)
+
+library(readr)
+library(janitor)
+library(tidyr)
+library(magrittr)
+url <- 'https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-09-01/key_crop_yields.csv'
+
+key_crop_yields <- read_csv(url) %>%
+  clean_names() %>%
+  pivot_longer(
+    cols = -___,
+    names_to = "crop",
+    values_to = "tonnes_per_hectare",
+    names_pattern = "([^_]+)"
+  )
